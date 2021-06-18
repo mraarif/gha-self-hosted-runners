87f0399b05d9b9e8b0434d6bbf5a7730d64e4e52
Aarif <mraarif@outlook.com>
2021-06-18 15:16:13 +0500

    changes

--- tests/tests_1.py
+++ tests/tests_1.py
@@ -1,6 +1,9 @@
 import pytest
+import time
 
 
 def test_divide_by_zero():
+    # mimicing really long tests run
+    time.sleep(30)
     with pytest.raises(ZeroDivisionError):
-        4 / 0
+        5 / 0
